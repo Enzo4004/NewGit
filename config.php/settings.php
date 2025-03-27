6d7b5db7538060bfcbeb5e628df3da7fcf53f1fb
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-03-27 10:49:30 +0100

    ajout du php et des base du php pour le header

---
+++ config.php/settings.php
@@ -0,0 +1,10 @@
+<?php
+//active l'affichage des erreurs    
+ini_set('display_errors , 1');
+
+
+//activate l' affichage des erreurs au démarrage 
+ini_set('display_startup_errors ,'1);
+
+//definit le niveau de rapport d'erreur à E_ALL (toutes les erreurs)
+error_reporting(E_ALL);
