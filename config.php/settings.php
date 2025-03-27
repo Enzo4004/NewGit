3f09179b7f2ae8a739f461615324772bac1a2a47
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-03-27 11:39:54 +0100

    ajout du boutton 'decouvvrir les artistes

--- config.php/settings.php
+++ config.php/settings.php
@@ -4,7 +4,7 @@ ini_set('display_errors , 1');
 
 
 //activate l' affichage des erreurs au démarrage 
-ini_set('display_startup_errors ,'1);
+ini_set('display_startup_errors ,1');
 
 //definit le niveau de rapport d'erreur à E_ALL (toutes les erreurs)
 error_reporting(E_ALL);
