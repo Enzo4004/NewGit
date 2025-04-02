e055b2f19106f828ba204084e0325e8020f4e84d
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-02 10:54:01 +0200

    vrai img

--- controllers/controllers_Header.control.php
+++
@@ -1,3 +0,0 @@
-
-<?php require_once("../config.php/settings.php");?>
-<?php  require_once("../view/view-Header.view.php");?>   
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-document.getElementById("button-1").addEventListener("click", function() {
+document.getElementsByClassName("button-1").addEventListener("click", function() {
     let button = this;
     
     // Ajout de la classe pour l'animation
     button.classList.add("animate");
 
     // Suppression après 3s pour revenir à l'état normal
-    setTimeout(() => {
+    Timeout(() => {
         button.classList.remove("animate");
     }, 300);
     console.log ("trés Bon choix !");
