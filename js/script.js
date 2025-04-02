6262a8387837dfe9af70c45962add0a5bb12823b
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-02 15:00:56 +0200

    ajout de la musique en fond dans la galeries

--- js/script.js
+++ js/script.js
@@ -10,3 +10,9 @@ document.getElementsByClassName("button-1").addEventListener("click", function()
     }, 300);
     console.log ("trés Bon choix !");
 });
+
+
+
+    //Gestion du volume 
+    var audio = document.querySelector("audio");
+    audio.volume = 0.1;  // Régle le volume à 50%
