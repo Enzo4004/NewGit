806335b062dc281da31fc41def94020332c94ca2
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-02 11:42:12 +0200

    img Galeries + debut du projet galeries

---
+++ view/view-Galeries.view.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Galeries</title>
+</head>
+<body>
+    <main>
+        <h1> <a href="#Galeries"></a>La Galeries !</h1>
+        <br>
+        <br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+</body>
+</html>
