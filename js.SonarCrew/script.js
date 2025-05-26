ec47aed60ca44fc32ca2db053b6859df9ce00a1e
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-05-26 12:07:38 +0200

    ajout de js et du pop up d'avertissement

---
+++ js.SonarCrew/script.js
@@ -0,0 +1,11 @@
+//Fonction pour ouvrir Le pop up 
+window.onload = fuction {
+    document.getElementById('popup').style.display = 'flex';
+
+};
+
+//fonction pour fermer le pop up 
+function.closePopup(){
+    document.getElementById('popup').style.display = 'none';   
+
+};
